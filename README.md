e4758ff2be749a5d015cd4f7be9003b316041f55
Lukasz Matysiewicz <mrlukaszmatysiewicz@gmail.com>
2025-02-20 21:42:55 +0100

    1.0.1 added Freemius
    1.0.1 added Freemius

---
+++ README.md
@@ -0,0 +1,7 @@
+
+- 1.0.1: Added Freemius functionality
+- 1.0.0: Initial release
+
+## Credits
+Created by Matysiewicz Studio
+Copyright (c) 2024 Matysiewicz Studio
--- animated-gutenberg-slider.php
+++ animated-gutenberg-slider.php
@@ -3,7 +3,7 @@
  * Plugin Name: Animated Gutenberg Slider
  * Plugin URI: https://matysiewicz.studio
  * Description: Create beautiful infinite sliders for Gutenberg columns
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Matysiewicz Studio
  * Author URI: https://matysiewicz.studio
  * License: GPL v2 or later
@@ -13,6 +13,40 @@
 
 if (!defined('WPINC')) {
     die;
+}
+if ( ! function_exists( 'ags_fs' ) ) {
+    // Create a helper function for easy SDK access.
+    function ags_fs() {
+        global $ags_fs;
+
+        if ( ! isset( $ags_fs ) ) {
+            // Include Freemius SDK.
+            require_once dirname( __FILE__ ) . '/vendor/freemius/start.php';
+            $ags_fs = fs_dynamic_init( array(
+                'id'                  => '17998',
+                'slug'                => 'animated-gutenberg-slider',
+                'type'                => 'plugin',
+                'public_key'          => 'pk_3ba60898184252cceb054c5e63b94',
+                'is_premium'          => true,
+                'is_premium_only'     => true,
+                'has_addons'          => false,
+                'has_paid_plans'      => true,
+                'menu'                => array(
+                    'contact'      => false,
+                    'support'      => false,
+                    'pricing'      => false,
+                    'account'      => true,
+                ),
+            ) );
+        }
+
+        return $ags_fs;
+    }
+
+    // Init Freemius.
+    ags_fs();
+    // Signal that SDK was initiated.
+    do_action( 'ags_fs_loaded' );
 }
 
 define('AGS_VERSION', '1.0.0');
